dd1eac46c0d7a711156bb395579fcba733bff8b7
Zhanel Nauryzbayeva <zhanelkakaramelka2000@gmail.com>
2026-04-17 13:41:33 +0500

    first commit

---
+++ src/main/java/com/example/springBootfirstapp/SpringBootfirstappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootfirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootfirstappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springBootfirstapp/SpringBootfirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootfirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
